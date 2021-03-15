c1605bb411d493592293baed7c0d57bd0d91fa8e
shabahat97 <umernasir19@gmail.com>
2021-03-15 18:12:00 +0500

    first commit

---
+++ Controllers/SiteController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Validations_DataAnnotations.Models;
+
+namespace Validations_DataAnnotations.Controllers
+{
+    public class SiteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+
+        }
+        public IActionResult RegisterUser()
+        {
+            return View();
+
+        }
+        [HttpPost]
+        public IActionResult RegisterUser(RegisterUserModel user)
+        {
+            if(!user.IsAgreedToTerms)
+            {
+                ModelState.AddModelError("IsAgreedToTerms", "Please check to proceed further.");
+            }
+            return View();
+
+        }
+        //[HttpPost]
+        //public IActionResult Index(EmployeeModel employee)
+        //{
+        //    return View();
+        //}
+
+
+    }
+}
---
+++ Models/EmployeeModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Validations_DataAnnotations.Models
+{
+    public class EmployeeModel
+    {
+        [Display(Name ="Full Name:")]
+        [Required(ErrorMessage ="Full Name is required.")]
+        public string FullName { get; set; }
+        [Display(Name = "Date of Birth:")]
+        [Required(ErrorMessage =   "Date Of Birth is required.")]
+
+        //[DataType(DataType.Date)]
+        //[DataType(DataType.Time)]
+        [DataType(DataType.DateTime)]
+        public DateTime DateOfBirth { get; set; }
+        [Display(Name = "Email:")]
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage ="Email is required")]
+
+
+
+        public string Email { get; set; }
+        [Display(Name = "Mobile:")]
+        [Required]
+
+        public string Mobile { get; set; }
+        [Display(Name = "Address:")]
+
+        public string Address { get; set; }
+        [Display(Name = "Salary:")]
+
+        public decimal? Salary { get; set; }
+        [Display(Name = "Website:")]
+
+        public string Website { get; set; }
+    }
+}
